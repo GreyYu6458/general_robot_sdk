4a9be006e032c81280921120154c71b4f643887d
chenhui.yu <chenhui.yu@inskylab.com>
2021-11-03 18:52:04 +0800

    add init value for sharedinfo

--- systems/dji_osdk_based/plugins/mission/DJIMissionSharedInfo.hpp
+++ systems/dji_osdk_based/plugins/mission/DJIMissionSharedInfo.hpp
@@ -4,15 +4,15 @@
 
 struct DJIMissionSharedInfo
 {
-    DJIWPMission                                dji_wp_mission;             // DJI 航线任务
+    DJIWPMission                                dji_wp_mission;                 // DJI 航线任务
 
-    uint32_t                                    total_wp;                   // 总共航点
+    uint32_t                                    total_wp{0};                   // 总共航点
 
-    uint32_t                                    current_repeated_times;     // 目前的重复次数
+    uint32_t                                    current_repeated_times{0};     // 目前的重复次数
 
-    uint32_t                                    total_repeated_times;       // 总共重复次数
+    uint32_t                                    total_repeated_times{0};       // 总共重复次数
 
-    std::vector<std::pair<uint64_t, uint32_t>>  photo_time_item_index_list;  // 照片事件时间和item_index的表
+    std::vector<std::pair<uint64_t, uint32_t>>  photo_time_item_index_list;     // 照片事件时间和item_index的表
 
     DJIMissionSharedInfo() = default;
 
