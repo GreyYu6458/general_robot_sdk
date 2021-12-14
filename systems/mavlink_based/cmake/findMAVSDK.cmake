84897a7bcbf3111f10516e0270479a65012cd9ba
chenhui.yu <chenhui.yu@inskylab.com>
2021-12-14 10:21:35 +0800

    debug commit

--- systems/mavlink_based/cmake/findMAVSDK.cmake
+++ systems/mavlink_based/cmake/findMAVSDK.cmake
@@ -19,6 +19,36 @@ ExternalProject_Add(
 
 SET(MAVSDK_LIBRARY_DIR ${MAVSDK_INSTALL_DIR}/lib)
 SET(MAVSDK_INCLUDE_DIR ${MAVSDK_INSTALL_DIR}/include)
+SET(
+    MAVSDK_LINK_LIBRARIES
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_action.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_action_server.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_calibration.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_camera.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_failure.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_follow_me.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_ftp.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_geofence.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_gimbal.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_log_files.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_manual_control.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_mission_raw_server.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_mocap.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_offboard.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_param.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_param_server.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_server_utility.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_shell.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_tracking_server.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_transponder.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_tune.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_info.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_telemetry.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_mission.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_mission_raw.a
+    ${MAVSDK_LIBRARY_DIR}/libmavsdk_mavlink_passthrough.a
+)
 
 link_directories(${MAVSDK_LIBRARY_DIR})
 include_directories(${MAVSDK_INCLUDE_DIR})
