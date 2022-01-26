6b1dcc03f6abad18ac2009fda5553e920d0e4805
chenhui.yu <chenhui.yu@inskylab.com>
2022-01-26 18:53:03 +0800

    cmake debug commit

--- cmake/super_build.cmake
+++ cmake/super_build.cmake
@@ -15,18 +15,20 @@ endif()
 # Pass down parameters
 # SUPERBUILD_ROOT_PATH must pass down
 if(NOT SUPERBUILD_ROOT_PATH)
-    set(SUPERBUILD_ROOT_PATH    ${CMAKE_BINARY_DIR}/superbuild/${CMAKE_BUILD_TYPE})
+    set(SUPERBUILD_ROOT_PATH ${CMAKE_BINARY_DIR}/superbuild)
     message("SUPER BUILD DEFAULT  ROOT DIRECTORY:${SUPERBUILD_ROOT_PATH}")
 else()
     message("SUPER BUILD CUSTOMER ROOT DIRECTORY:${SUPERBUILD_ROOT_PATH}")
 endif()
+
+set(SUPERBUILD_BUILD_TYPE_PATH  SUPERBUILD_ROOT_PATH/${CMAKE_BUILD_TYPE})
+set(SUPERBUILD_WORK_PATH        ${SUPERBUILD_BUILD_TYPE_PATH}/ws)
 
 # Default install prefix set to binary directory
 # CMAKE_INSTALL_PREFIX must pass down
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     set(
         CMAKE_INSTALL_PREFIX 
-        ${SUPERBUILD_ROOT_PATH}/install
         CACHE  STRING 
         "SET CMAKE INSTALL PREFIX" FORCE
     )
@@ -47,44 +49,47 @@ message("SUPER BUILD CMAKE PREFIX PATH:${CMAKE_PREFIX_PATH}")
 
 include_directories(${CMAKE_INSTALL_PREFIX}/include)
 
+
 function(SET_SUPERBUILD TARGET_NAME CMAKE_CONFIG_PATH)
 
-    set(BUILD_DONE_NAME ${TARGET_NAME}_BUILD_DONE)
+    set(options             "placeholder_opt")
+    set(one_value_args      "placeholder_one_value")
+    set(multi_value_args    "CMAKE_ARGS")
+    set(BUILD_DONE_NAME     ${TARGET_NAME}_${CMAKE_BUILD_TYPE}_BUILD_DONE)
+    set(TARGET_SOURCE_DIR  "${CMAKE_CONFIG_PATH}")
+    set(TARGET_BINARY_DIR  "${SUPERBUILD_WOKD_PATH}/${TARGET_NAME}")
 
     if(DEFINED ${BUILD_DONE_NAME})
         if(${BUILD_DONE_NAME})
             message("SUPER BUILD HAD ALREADY BUILT: ${TARGET_NAME}")
             return()
+        else()
+            message("SUPER BUILD START: ${TARGET_NAME}")
         endif()
     endif()
-
-    message("SUPER BUILD START: ${TARGET_NAME}")
-
-    set(TARGET_SOURCE_DIR  "${CMAKE_CONFIG_PATH}")
-    set(TARGET_BINARY_DIR  "${SUPERBUILD_ROOT_PATH}/ws/${TARGET_NAME}")
 
     file(MAKE_DIRECTORY ${TARGET_BINARY_DIR})
 
-    if(NOT ${ARGC} LESS 3)
-        if(${ARGV2} STREQUAL "CMAKE_ARGS")
-            set(CUSTOM_CONFIG_COMMAND)
-            foreach(ARG_NUM RANGE 3 ${ARGC})
-                list(APPEND CUSTOM_CONFIG_COMMAND ${ARGV${ARG_NUM}})
-            endforeach()
-        endif()
-    endif()
+    list(SUBLIST ARGN 2 -1 LIST_ARGN)
+    cmake_parse_arguments(
+        PARSE_RESULT
+        ${options} 
+        ${one_value_args} 
+        ${multi_value_args}
+        ${LIST_ARGN}
+    )
 
     execute_process(
         COMMAND ${CMAKE_COMMAND}
             # "-G${CMAKE_GENERATOR}"
             "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
             "-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}"
-            "-DSUPER_BUILD_CUSTOMER_COMMAND=${CUSTOM_CONFIG_COMMAND}" # CUSTOMER CONFIG COMMAND
+            "-DSUPER_BUILD_CUSTOMER_COMMAND=${PARSE_RESULT_CMAKE_ARGS}" # CUSTOMER CONFIG COMMAND
             # SUPER BUILD CONFIG PARAMETERS PASS DOWN
             "-DSUPERBUILD_ROOT_PATH=${SUPERBUILD_ROOT_PATH}"
             "${TARGET_SOURCE_DIR}"
         WORKING_DIRECTORY 
-            "${TARGET_BINARY_DIR}"
+            "${SUPERBUILD_WORK_PATH}"
         RESULT_VARIABLE CONFIGURE_FAILED
     )
 
@@ -97,19 +102,19 @@ function(SET_SUPERBUILD TARGET_NAME CMAKE_CONFIG_PATH)
     if(MSVC)
         execute_process(
             COMMAND ${CMAKE_COMMAND} --build . --parallel ${NUM_PROCS}  --config ${CMAKE_BUILD_TYPE}
-            WORKING_DIRECTORY ${TARGET_BINARY_DIR}
+            WORKING_DIRECTORY ${SUPERBUILD_WORK_PATH}
             RESULT_VARIABLE BUILD_FAILED
         )
     else()
         execute_process(
             COMMAND ${CMAKE_COMMAND} --build . --parallel ${NUM_PROCS}
-            WORKING_DIRECTORY ${TARGET_BINARY_DIR}
+            WORKING_DIRECTORY ${SUPERBUILD_WORK_PATH}
             RESULT_VARIABLE BUILD_FAILED
         )
     endif()
 
     if(${BUILD_FAILED})
-        message(FATAL_ERROR "${TARGET_BINARY_DIR} failed to build!")
+        message(FATAL_ERROR "${SUPERBUILD_WORK_PATH} failed to build!")
     else()
         message("SUPER BUILD SUCCESS: ${TARGET_NAME}, ${BUILD_DONE_NAME} will be set to true")
         set(${BUILD_DONE_NAME} TRUE CACHE BOOL "Did ${TARGET_NAME} has built" FORCE)
